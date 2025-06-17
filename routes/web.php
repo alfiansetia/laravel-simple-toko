206bdc8d930cf46ab80159981935b957ec825bde
Alfian Setiawan <56498155+alfiansetia@users.noreply.github.com>
2025-06-17 16:35:15 +0700

    template

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,11 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
