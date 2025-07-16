0ab25acc35025087ed48c2d3604215faf487cb03
Alfian Setiawan <56498155+alfiansetia@users.noreply.github.com>
2025-07-16 11:50:12 +0700

    Update TransactionResource.php

--- app/Filament/Resources/TransactionResource.php
+++ app/Filament/Resources/TransactionResource.php
@@ -162,7 +162,7 @@ class TransactionResource extends Resource
     {
         return [
             'index' => Pages\ListTransactions::route('/'),
-            'create' => Pages\CreateTransaction::route('/create'),
+            // 'create' => Pages\CreateTransaction::route('/create'),
             'edit' => Pages\EditTransaction::route('/{record}/edit'),
         ];
     }
